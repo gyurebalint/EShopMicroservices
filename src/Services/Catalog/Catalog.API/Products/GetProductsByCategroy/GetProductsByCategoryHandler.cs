51d18db16398b5ce2b0f92ced5a4b5b006d06fb6
gyurebalint <gyurebalint@gmail.com>
2025-03-08 13:51:40 +0100

    Catalog API CQRS all CRUD is done

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategroy/GetProductsByCategoryHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductsByCategroy
+{
+    public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+    public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+    {
+        public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync();
+
+            return new GetProductsByCategoryResult(products);
+        }
+    }
+}
